ec2174f63354a65956de1f9af54c0aeeda0813c8
DFENS01 <47895152+DFENS01@users.noreply.github.com>
2019-02-22 16:40:36 +0100

    Version 01

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>02 Site</title>
+
+  <link rel="shortcut icon" href="http://php.net/favicon.ico">
+  <link rel="stylesheet" type="text/css" href="site02.css">
+  
+</head>
+<body>
+    <h1>02 Site</h1>
+</body>
+</html>
